c065b1f2a3f0fd777eb71219b7c29cbb575355c3
bharat20061999 <bharatsirvi35@gmail.com>
2023-10-01 16:57:17 +0530

    js variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,9 @@
+const userId = 123;
+let userName = "bharatChoudhary";
+var userEmail = "bharat@google.com";
+ userCity = "bali";
+
+ 
+
+//please do not use var beacuse of block scope and functional scope.
+console.table([userId,userEmail,userName,userCity]);
