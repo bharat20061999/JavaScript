5285d44b7219e21ec4e2836fd3beccadbe535edb
bharat20061999 <bharatsirvi35@gmail.com>
2023-10-02 09:36:26 +0530

    dataTypes

---
+++ 01_basics/dataType.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all JS code as newer version
+
+//alert ( 3 + 3); // we are using nodejs , not broswer
+
+let name = "bharat"
+let age = 18
+let isLoggedIn = false
+
+
+//number => 2 to 53
+//bigint for large values (fb, reddit)
+//string => ""
+//null => stanalone value(representation)
+//undefined => 
+//symbol => unique
+//object =>>
+
+console.log(typeof "bharat");
+console.log(typeof null);  // it will give object
+console.log(typeof undefined); // it will give undefined
