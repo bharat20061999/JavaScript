95f0b190ccbe761871657d233e3298f0bc88e6b0
bharat20061999 <bharatsirvi35@gmail.com>
2023-10-02 09:51:02 +0530

    conversion of dataType

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "55";
+
+console.log( typeof score);
+
+let valuesInNumber = Number(score)
+console.log(typeof valuesInNumber);
+console.log(valuesInNumber);
+
+
+// "55" => 33
+// "33adsf" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
